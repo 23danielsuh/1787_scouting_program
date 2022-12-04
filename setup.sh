6b0528b2662c112f474232f776a5013ef42a2a90
Daniel Suh <63919918+23danielsuh@users.noreply.github.com>
2022-12-04 12:03:12 -0500

    delete files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-python3 -m venv env
-source env/bin/activate
-
-touch requirements.txt
-echo "google-api-python-client==1.7.9" >> requirements.txt
-echo "google-auth-httplib2==0.0.3" >> requirements.txt
-echo "google-auth-oauthlib==0.4.0" >> requirements.txt
-
-pip3 install -r requirements.txt
