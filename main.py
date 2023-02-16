ba2c9a5a617155d02afa909a17ce1809b8f8aa36
Daniel Suh <63919918+23danielsuh@users.noreply.github.com>
2023-02-15 23:57:23 -0500

    add stats, next is writing to excel

--- main.py
+++ main.py
@@ -5,26 +5,45 @@ import xlsxwriter
 from colour import Color
 import numpy as np
 
-def get_total_points(grid_info):
-    ret_df = pd.Series(np.zeros((len(grid_info))))
 
-    for name in grid_info.columns:
-        column = grid_info[name]
+def get_total_points(df):
+    aggregate_columns = [
+        "auto_points",
+        "total_points",
+        "num_cycles",
+        "charge_station_points",
+    ]
+    ret_df = pd.DataFrame(
+        np.zeros((len(df), len(aggregate_columns))), columns=aggregate_columns
+    )
+
+    for name in df.columns:
+        if "cone" not in name and "cube" not in name:
+            continue
+
+        column = df[name]
         score = 0
 
-        if 'high' in name:
+        if "high" in name:
             score = 5
-        if 'mid' in name:
+        if "mid" in name:
             score = 3
-        if 'low' in name:
+        if "low" in name:
             score = 2
-        if 'auto' in name:
-            score += 1
 
-        ret_df += score * column
+        if "auto" in name:
+            score += 1
+            ret_df["auto_points"] += score * column
+        else:
+            ret_df["num_cycles"] += 1 * column
+
+        ret_df["total_points"] += score * column
+
+    ret_df["total_points"] += df["auto_balance"] + df["tele_balance"]
+    ret_df["auto_points"] += df["auto_balance"]
+    ret_df["charge_station_points"] += df["auto_balance"] + df["tele_balance"]
 
     return ret_df
-
 
 
 def create_categories(df):
@@ -41,7 +60,9 @@ def create_categories(df):
 
     df["timestamp"] = dates
 
-    df[['team_number', 'match_number']] = df[['team_number', 'match_number']].astype(int)
+    df[["team_number", "match_number"]] = df[["team_number", "match_number"]].astype(
+        int
+    )
 
     grid_columns = [
         "auto_cone_high",
@@ -59,25 +80,22 @@ def create_categories(df):
     ]
 
     df[grid_columns] = df[grid_columns].astype(str)
-    df[grid_columns] = df[grid_columns].applymap(
-        lambda x: max(map(int, x.split(", ")))
-    )
+    df[grid_columns] = df[grid_columns].applymap(lambda x: max(map(int, x.split(", "))))
     df[grid_columns] = df[grid_columns].astype(int)
 
-    balancing_columns = [
-        'auto_balance',
-        'tele_balance',
-    ]
-    df[balancing_columns] = df[balancing_columns].replace({'Yes, balanced': 'balanced', 'Yes, unbalanced': 'unbalanced', 'No': 'none'})
+    df["auto_balance"] = df["auto_balance"].replace(
+        {"Yes, balanced": 12, "Yes, unbalanced": 8, "No": 0}
+    )
+    df["tele_balance"] = df["tele_balance"].replace(
+        {"Yes, balanced": 10, "Yes, unbalanced": 6, "No": 0}
+    )
 
-    df['total_points'] = get_total_points(df[grid_columns])
-    print(df['total_points'])
-    df['num_cycles'] = df[grid_columns].sum()
+    df = df.join(get_total_points(df))
 
     return df
 
 
-def get_dataframe(path, min_points):
+def get_team_dfs(path, min_points):
     columns = [
         "timestamp",
         "name",
@@ -107,9 +125,52 @@ def get_dataframe(path, min_points):
     df.columns = columns
 
     df = create_categories(df)
-    # df = filter_dataframe(df, min_points)
+    teams = df["team_number"].unique()
 
-    return df
+    teams.sort()
+    ret = []
+
+    for team in teams:
+        if df.loc[df["team_number"] == team]["total_points"].mean() >= min_points:
+            ret.append(team)
+
+    return ret, df
+
+
+def get_statistics(df, teams):
+    stats_columns = [
+        "team_number",
+        "average_total_points",
+        "average_auto_points",
+        "average_num_cycles",
+        "average_charge_station_points",
+        "lsrl_slope",
+        "defense_percentage",
+        "p_value",
+    ]
+    stats_df = pd.DataFrame(
+        np.zeros((len(teams), len(stats_columns))), columns=stats_columns
+    )
+
+    for team in teams:
+        stats_df["team_number"] = team
+        stats_df[
+            [
+                "average_total_points",
+                "average_auto_points",
+                "average_num_cycles",
+                "average_charge_station_points",
+            ]
+        ] = (
+            df.loc[df["team_number"] == team][
+                ["total_points", "auto_points", "num_cycles", "charge_station_points"]
+            ]
+            .mean(axis=0)
+            .round(2)
+        )
+
+    return stats_df
+
 
 def process_args():
     parser = argparse.ArgumentParser(
@@ -132,14 +193,16 @@ def process_args():
     args = parser.parse_args()
 
     return args
-    
+
 
 def main():
     # adds flags
     args = process_args()
 
-    df = get_dataframe(args.path, args.min_points)
-    df.to_csv('wtf.csv')
+    teams, df = get_team_dfs(args.path, args.min_points)
+    df.to_csv("info.csv")
+    stats_df = get_statistics(df, teams)
+    stats_df.to_csv("stats.csv")
 
     colors = list(Color("orange").range_to(Color("grey"), len(df)))
 
